e82763ab46246f6932de1c72c08ad71bbdbe5feb
KULDEEP RATNU <45199413+kuldeepratnu@users.noreply.github.com>
2021-10-18 08:36:20 +0530

    passing for all integer test cases

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,9 +3,12 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        if(input.equals("1"))
-        	return 1;
-        return 0;
+    	try{
+            int result = Integer.parseInt(input);
+            return result;
+        }catch(Exception e ){
+            return 0;
+        } 
     }
 
 }
