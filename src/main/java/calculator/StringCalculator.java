bea54bfc8fd38bce52c1e748aadec682e8ea8940
KULDEEP RATNU <45199413+kuldeepratnu@users.noreply.github.com>
2021-10-18 09:01:51 +0530

    all test cases are passed. done with the assessment.

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,13 +2,35 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) {
-    	try{
-            int result = Integer.parseInt(input);
-            return result;
-        }catch(Exception e ){
-            return 0;
-        } 
+    public int add(String input) 
+    {
+    	/* Checking for  empty input string */
+        if(input.isEmpty())
+        	return 0;
+        else if(input.length() > 1)
+        {
+        	/* Split the string with delimiters ',' or '\n' and store in array */
+        	String[] arrayOfNumbers = input.split(",|\n");
+        	
+        	return sumOfNumbericString(arrayOfNumbers);
+        }
+        else
+        {
+        	/* Return the string as integer when string has 1 digit */
+        	return Integer.parseInt(input);
+        }
     }
-
+    
+    /* To sum the numbers in string passed*/
+    int sumOfNumbericString(String[] stringOfNumbers)
+    {
+    	int result = 0;
+    	/* Convert each number in string to integer and add to sum */
+    	for(String number:stringOfNumbers)
+    	{
+    		result += Integer.parseInt(number);
+    	}
+    	
+    	return result;
+    }
 }
